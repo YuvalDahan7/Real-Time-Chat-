9272c0d16eeb93c6e074bc9d227b446048f64aef
Yuval Dahan <dahan236@gmail.com>
2024-03-17 17:42:35 +0200

    last commit

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -11,5 +11,6 @@ router.post("/login", login);
 router.post("/register", register);
 router.get("/allusers/:id", getAllUsers);
 router.get("/logout/:id", logOut);
+// router.get("/game/:id", game)
 
 module.exports = router;
